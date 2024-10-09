93220095ec9fffaf4dcebbe7ab2f0b7e10efe6a1
Daniel Bueno Navarro <danielbuenonavarro@gmail.com>
2024-10-09 14:30:15 +0200

    falta buildear

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,5 +1,4 @@
 import clsx, { ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export const cn = (...classes: ClassValue[]) => twMerge(clsx(...classes));
-
+export const cn = (...classes: ClassValue[]) => twMerge(clsx(classes));
